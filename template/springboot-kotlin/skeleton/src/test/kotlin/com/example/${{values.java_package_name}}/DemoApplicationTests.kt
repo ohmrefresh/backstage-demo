090eb76f8936daed08ba67d4d316910178fce81e
Authapon Kongkaew <ohmrefresh@gmail.com>
2022-10-01 14:15:54 +0700

    first commit

---
+++ template/springboot-kotlin/skeleton/src/test/kotlin/com/example/${{values.java_package_name}}/DemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.${{values.java_package_name}}
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
